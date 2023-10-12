f0ff54a9a18a2b1d3078c595676ed088d6ad1a70
‘aja <aja.onyebuchi@yahoo.com>
2023-10-12 12:02:00 +0100

    create project and packages

---
+++ src/test/java/com/bujubanton/transactionmamagement/TransactionMamagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bujubanton.transactionmamagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionMamagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
