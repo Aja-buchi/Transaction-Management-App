c824cb1732ee380299984dc711f43dfbccc7a515
‘aja <aja.onyebuchi@yahoo.com>
2023-10-16 11:37:59 +0100

    create order and payment entity

---
+++ src/main/java/com/bujubanton/transactionmamagement/entity/Payment.java
@@ -0,0 +1,22 @@
+package com.bujubanton.transactionmamagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String type;
+    private String cardName;
+    private String cardNumber;
+    private int expiryYear;
+    private int expiryMonth;
+    private int cvc;
+    private Long orderId;
+}
